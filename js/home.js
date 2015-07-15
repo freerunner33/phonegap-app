65cfe7e82a69dc37c4883e464c5e981d129975b5
Noah Thomas <noahbthomas@sandiego.edu>
2015-07-15 02:56:44 -0600

    Change menu opacity

--- js/home.js
+++ js/home.js
@@ -3,6 +3,9 @@ document.addEventListener('deviceready', function() {
 	$("div#header img").click(function() {
 		$("div#menu").toggleClass("active")
 	})
+	$("div#overlay").click(function() {
+		$("div#menu").removeClass("active")
+	})
 }, false);
 
 function imageFunction() {
