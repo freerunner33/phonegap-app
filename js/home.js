3a1ad0c1458eaed38c35de26c89cd592b98c242e
Noah Thomas <noahbthomas@sandiego.edu>
2015-07-15 12:00:51 -0600

    Overlay test2

--- js/home.js
+++ js/home.js
@@ -3,9 +3,6 @@ document.addEventListener('deviceready', function() {
 	$("div#header img").click(function() {
 		$("div#menu").toggleClass("active")
 	})
-	$("div#overlay").click(function() {
-		$("div#menu").toggleClass("active")
-	})
 }, false);
 
 $(document).ready(function() {
