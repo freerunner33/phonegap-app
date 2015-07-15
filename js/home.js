2930c747735cbb424fb70294b8265d1b2e6a2390
Noah Thomas <noahbthomas@sandiego.edu>
2015-07-15 12:53:28 -0600

    Overlay slides in

--- js/home.js
+++ js/home.js
@@ -2,8 +2,10 @@
 document.addEventListener('deviceready', function() {
 	$("div#header img").click(function() {
 		$("div#menu").toggleClass("active")
+		$("div#overlay").toggleClass("active")
 	})
 	$("div#overlay").click(function() {
 		$("div#menu").toggleClass("active")
+		$("div#overlay").toggleClass("active")
 	})
 }, false)
