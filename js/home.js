6473745df016619fa1e8b8b5ac770651ee885190
Noah Thomas <noahbthomas@sandiego.edu>
2015-07-15 03:02:35 -0600

    No click for overlay

--- js/home.js
+++ js/home.js
@@ -1,9 +1,5 @@
 
 document.addEventListener('deviceready', function() {
-	
-	$("div#overlay").click(function() {
-		$("div#menu").removeClass("active")
-	})
 	$("div#header img").click(function() {
 		$("div#menu").toggleClass("active")
 	})
