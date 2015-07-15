61a3613222fdaad5df35fc052c37b2849d15d1fa
Noah Thomas <noahbthomas@sandiego.edu>
2015-07-15 02:59:10 -0600

    testing overlay stuff

--- js/home.js
+++ js/home.js
@@ -1,10 +1,11 @@
 
 document.addEventListener('deviceready', function() {
-	$("div#header img").click(function() {
-		$("div#menu").toggleClass("active")
-	})
+	
 	$("div#overlay").click(function() {
 		$("div#menu").removeClass("active")
+	})
+	$("div#header img").click(function() {
+		$("div#menu").toggleClass("active")
 	})
 }, false);
 
