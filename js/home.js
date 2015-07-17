ebec74ea621504f0b1591d46e4a518e6c9659a37
Noah Thomas <noahbthomas@sandiego.edu>
2015-07-17 12:51:08 -0600

    Added all restaurants and modified footer

--- js/home.js
+++ js/home.js
@@ -60,31 +60,23 @@ document.addEventListener('deviceready', function() {
 	})
 }, false)
 
-// $(document).ready(function() {
-// 	$("div#header img").click(function() {
-// 		$("div#menu").toggleClass("active")
-// 		$("div#overlay").toggleClass("active")
-// 	})
-// 	$("div#overlay").click(function() {
-// 		$("div#menu").toggleClass("active")
-// 		$("div#overlay").toggleClass("active")
-// 	})
-// 	$("div#dining>div").click(function() {
-// 		open(dining[$(this).attr('id')].page, "_self")
-// 	})
+$(document).ready(function() {
+	$("div#header img").click(function() {
+		$("div#menu").toggleClass("active")
+		$("div#overlay").toggleClass("active")
+	})
+	$("div#overlay").click(function() {
+		$("div#menu").toggleClass("active")
+		$("div#overlay").toggleClass("active")
+	})
+	$("div#dining>div").click(function() {
+		open(dining[$(this).attr('id')].page, "_self")
+	})
 
-// 	$("div#main section").on("click", function() {
-// 		if ($(this).attr("id") == "about") {
-// 			$(this).removeClass("active")
-// 			$("div#main section#info").addClass("active")
-// 		}
-// 		else if ($(this).attr("id") == "info") {
-// 			$(this).removeClass("active")
-// 			$("div#main section#menu").addClass("active")
-// 		}
-// 		else {
-// 			$(this).removeClass("active")
-// 			$("div#main section#about").addClass("active")
-// 		}
-// 	})
-// })
+	$("div#main nav a").on("click", function() {
+		$("div#main section.active").removeClass("active")
+		$("div#main nav a.active").removeClass("active")
+		$("div#main section#" + $(this).attr("id")).addClass("active")
+		$("div#main nav a#" + $(this).attr("id")).addClass("active")
+	})
+})
