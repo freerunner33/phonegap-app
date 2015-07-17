439673fabd4bf791fa9766f0fea5afe9d4cbea2b
Noah Thomas <noahbthomas@sandiego.edu>
2015-07-17 13:38:44 -0600

    Testing menu

--- js/home.js
+++ js/home.js
@@ -44,13 +44,7 @@ document.addEventListener('deviceready', function() {
 		open(dining[$(this).attr('id')].page, "_self")
 	})
 
-	$("div#main nav a").on("click", function() {
-		alert("Hi")
-		$("div#main section.active").removeClass("active")
-		$("div#main nav a.active").removeClass("active")
-		$("div#main section#" + $(this).attr("id")).addClass("active")
-		$("div#main nav a#" + $(this).attr("id")).addClass("active")
-	})
+	
 }, false)
 
 $(document).ready(function() {
