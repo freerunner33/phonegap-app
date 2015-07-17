8ca394306278e44ce8f4b9df140dc28df1368b42
Noah Thomas <noahbthomas@sandiego.edu>
2015-07-17 12:53:20 -0600

    Same without document code for js

--- js/home.js
+++ js/home.js
@@ -60,23 +60,23 @@ document.addEventListener('deviceready', function() {
 	})
 }, false)
 
-$(document).ready(function() {
-	$("div#header img").click(function() {
-		$("div#menu").toggleClass("active")
-		$("div#overlay").toggleClass("active")
-	})
-	$("div#overlay").click(function() {
-		$("div#menu").toggleClass("active")
-		$("div#overlay").toggleClass("active")
-	})
-	$("div#dining>div").click(function() {
-		open(dining[$(this).attr('id')].page, "_self")
-	})
+// $(document).ready(function() {
+// 	$("div#header img").click(function() {
+// 		$("div#menu").toggleClass("active")
+// 		$("div#overlay").toggleClass("active")
+// 	})
+// 	$("div#overlay").click(function() {
+// 		$("div#menu").toggleClass("active")
+// 		$("div#overlay").toggleClass("active")
+// 	})
+// 	$("div#dining>div").click(function() {
+// 		open(dining[$(this).attr('id')].page, "_self")
+// 	})
 
-	$("div#main nav a").on("click", function() {
-		$("div#main section.active").removeClass("active")
-		$("div#main nav a.active").removeClass("active")
-		$("div#main section#" + $(this).attr("id")).addClass("active")
-		$("div#main nav a#" + $(this).attr("id")).addClass("active")
-	})
-})
+// 	$("div#main nav a").on("click", function() {
+// 		$("div#main section.active").removeClass("active")
+// 		$("div#main nav a.active").removeClass("active")
+// 		$("div#main section#" + $(this).attr("id")).addClass("active")
+// 		$("div#main nav a#" + $(this).attr("id")).addClass("active")
+// 	})
+// })
