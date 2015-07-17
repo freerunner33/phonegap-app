53afad56b7a6afeb5d1f57d32c3e77682e3b7fcd
Noah Thomas <noahbthomas@sandiego.edu>
2015-07-17 12:57:00 -0600

    Phone code copied

--- js/home.js
+++ js/home.js
@@ -1,36 +1,4 @@
 
-var dining = {
-	aromas: {
-		title: "Aroma's Cafe",
-		description: "Located in the heart of Alcala Park, adjacent to the Immaculata, is USD’s award-winning coffeehouse. " +
-			"Always abuzz with activity, Aromas is the perfect setting to enjoy a beverage with friends without having to leave " +
-			"campus.</p><p>We are proud to serve local, alumni-owned, Ryan Bros. Coffee. Choose from an array of espresso " +
-			"beverages, teas, tea lattes, savory and sweet crepes made fresh to order, blended drinks, gourmet baked goods and " +
-			"salads. We also have complimentary WiFi and computers available for your use.",
-		page: "aromas.html"
-	},
-	berts: {
-		title: "Bert's Bistro",
-		description: "Fantastic for lunch",
-		page: "berts.html"
-	},
-	bluespoon: {
-		title: "Blue Spoon",
-		description: "Apparently serves good stuff",
-		page: "bluespoon.html"
-	},
-	lagranterraza: {
-		title: "La Gran Terraza",
-		description: "I'm so fancy",
-		page: "lagranterraza.html"
-	},
-	lapaloma: {
-		title: "La Paloma",
-		description: "Great restaurant for breakfast and lunch.",
-		page: "lapaloma.html"
-	}
-}
-
 document.addEventListener('deviceready', function() {
 	$("div#header img").click(function() {
 		$("div#menu").toggleClass("active")
@@ -44,19 +12,11 @@ document.addEventListener('deviceready', function() {
 		open(dining[$(this).attr('id')].page, "_self")
 	})
 
-	$("div#main section").on("click", function() {
-		if ($(this).attr("id") == "about") {
-			$(this).removeClass("active")
-			$("div#main section#info").addClass("active")
-		}
-		else if ($(this).attr("id") == "info") {
-			$(this).removeClass("active")
-			$("div#main section#menu").addClass("active")
-		}
-		else {
-			$(this).removeClass("active")
-			$("div#main section#about").addClass("active")
-		}
+	$("div#main nav a").on("click", function() {
+		$("div#main section.active").removeClass("active")
+		$("div#main nav a.active").removeClass("active")
+		$("div#main section#" + $(this).attr("id")).addClass("active")
+		$("div#main nav a#" + $(this).attr("id")).addClass("active")
 	})
 }, false)
 
