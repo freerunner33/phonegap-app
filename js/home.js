73aab98a656e2c45f2e1e15dd9cddcbc89cdcc10
Noah Thomas <noahbthomas@sandiego.edu>
2015-07-17 13:08:33 -0600

    Updated phone code

--- js/home.js
+++ js/home.js
@@ -1,4 +1,36 @@
 
+var dining = {
+	aromas: {
+		title: "Aroma's Cafe",
+		description: "Located in the heart of Alcala Park, adjacent to the Immaculata, is USD’s award-winning coffeehouse. " +
+			"Always abuzz with activity, Aromas is the perfect setting to enjoy a beverage with friends without having to leave " +
+			"campus.</p><p>We are proud to serve local, alumni-owned, Ryan Bros. Coffee. Choose from an array of espresso " +
+			"beverages, teas, tea lattes, savory and sweet crepes made fresh to order, blended drinks, gourmet baked goods and " +
+			"salads. We also have complimentary WiFi and computers available for your use.",
+		page: "aromas.html"
+	},
+	berts: {
+		title: "Bert's Bistro",
+		description: "Fantastic for lunch",
+		page: "berts.html"
+	},
+	bluespoon: {
+		title: "Blue Spoon",
+		description: "Apparently serves good stuff",
+		page: "bluespoon.html"
+	},
+	lagranterraza: {
+		title: "La Gran Terraza",
+		description: "I'm so fancy",
+		page: "lagranterraza.html"
+	},
+	lapaloma: {
+		title: "La Paloma",
+		description: "Great restaurant for breakfast and lunch.",
+		page: "lapaloma.html"
+	}
+}
+
 document.addEventListener('deviceready', function() {
 	$("div#header img").click(function() {
 		$("div#menu").toggleClass("active")
@@ -20,23 +52,23 @@ document.addEventListener('deviceready', function() {
 	})
 }, false)
 
-// $(document).ready(function() {
-// 	$("div#header img").click(function() {
-// 		$("div#menu").toggleClass("active")
-// 		$("div#overlay").toggleClass("active")
-// 	})
-// 	$("div#overlay").click(function() {
-// 		$("div#menu").toggleClass("active")
-// 		$("div#overlay").toggleClass("active")
-// 	})
-// 	$("div#dining>div").click(function() {
-// 		open(dining[$(this).attr('id')].page, "_self")
-// 	})
+$(document).ready(function() {
+	$("div#header img").click(function() {
+		$("div#menu").toggleClass("active")
+		$("div#overlay").toggleClass("active")
+	})
+	$("div#overlay").click(function() {
+		$("div#menu").toggleClass("active")
+		$("div#overlay").toggleClass("active")
+	})
+	$("div#dining>div").click(function() {
+		open(dining[$(this).attr('id')].page, "_self")
+	})
 
-// 	$("div#main nav a").on("click", function() {
-// 		$("div#main section.active").removeClass("active")
-// 		$("div#main nav a.active").removeClass("active")
-// 		$("div#main section#" + $(this).attr("id")).addClass("active")
-// 		$("div#main nav a#" + $(this).attr("id")).addClass("active")
-// 	})
-// })
+	$("div#main nav a").on("click", function() {
+		$("div#main section.active").removeClass("active")
+		$("div#main nav a.active").removeClass("active")
+		$("div#main section#" + $(this).attr("id")).addClass("active")
+		$("div#main nav a#" + $(this).attr("id")).addClass("active")
+	})
+})
