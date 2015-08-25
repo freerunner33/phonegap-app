ba01c236ca9006b3b17ae96ae27725123b17cced
Noah Thomas <noahbthomas@sandiego.edu>
2015-08-25 11:51:45 -0700

    Using array to fill menus

--- js/home.js
+++ js/home.js
@@ -206,6 +206,8 @@ var menu = [
 
 
 document.addEventListener('deviceready', function() {
+	fillTables()
+	
 	$("div#header img").click(function() {
 		$("div#menu").toggleClass("active")
 		$("div#overlay").toggleClass("active")
@@ -229,31 +231,6 @@ document.addEventListener('deviceready', function() {
 		$(this).next("div#content").toggleClass("active")
 	})
 }, false)
-
-// $(document).ready(function() {
-// 	$("div#header img").click(function() {
-// 		$("div#menu").toggleClass("active")
-// 		$("div#overlay").toggleClass("active")
-// 	})
-// 	$("div#overlay").click(function() {
-// 		$("div#menu").toggleClass("active")
-// 		$("div#overlay").toggleClass("active")
-// 	})
-// 	$("div#dining>div").click(function() {
-// 		open(dining[$(this).attr('id')].page, "_self")
-// 	})
-
-// 	$("div#main nav a").click(function() {
-// 		$("div#main section.active").removeClass("active")
-// 		$("div#main nav a.active").removeClass("active")
-// 		$("div#main section#" + $(this).attr("id")).addClass("active")
-// 		$("div#main nav a#" + $(this).attr("id")).addClass("active")
-// 	})
-
-// 	$("div#collapsible h3").click(function() {
-// 		$(this).next("div#content").toggleClass("active")
-// 	})
-// })
 
 
 // $(document).ready(function() {
