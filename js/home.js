95e0c32f0ac6f8e2d107bc098ce15ee5c2d54068
Noah Thomas <noahbthomas@sandiego.edu>
2015-08-25 11:25:46 -0700

    Should fully work at this pt

--- js/home.js
+++ js/home.js
@@ -53,5 +53,30 @@ document.addEventListener('deviceready', function() {
 	})
 }, false)
 
+// $(document).ready(function() {
+// 	$("div#header img").click(function() {
+// 		$("div#menu").toggleClass("active")
+// 		$("div#overlay").toggleClass("active")
+// 	})
+// 	$("div#overlay").click(function() {
+// 		$("div#menu").toggleClass("active")
+// 		$("div#overlay").toggleClass("active")
+// 	})
+// 	$("div#dining>div").click(function() {
+// 		open(dining[$(this).attr('id')].page, "_self")
+// 	})
+
+// 	$("div#main nav a").click(function() {
+// 		$("div#main section.active").removeClass("active")
+// 		$("div#main nav a.active").removeClass("active")
+// 		$("div#main section#" + $(this).attr("id")).addClass("active")
+// 		$("div#main nav a#" + $(this).attr("id")).addClass("active")
+// 	})
+
+// 	$("div#collapsible h3").click(function() {
+// 		$(this).next("div#content").toggleClass("active")
+// 	})
+// })
+
 
 
