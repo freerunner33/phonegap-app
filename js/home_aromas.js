0acf0da0acd8f9063b5207e2f5bb1b6b25c1939d
--unset <--unset>
2015-08-28 23:56:05 -0700

    Back to old code

--- js/home_aromas.js
+++ js/home_aromas.js
@@ -65,43 +65,39 @@ function fillTables() {
 					price5: item.price5
 				})
 			})
-			alert("First menu size is " + menu.length)
 		},
 		error: function() {
 			output.append('<p>Please connect to an internet connection for up to date dining information</p>')
 		},
 		complete: function() {
-			alert("Second menu size is " + menu.length)
-			if (menu != null) {
-				$.each(categories, function(i, cat) {
-					var str = '<div id="collapsible"><h3>' + cat.name + '</h3><div id="content">'
-					str += '<table><tr>'
-					str += '<th>Name</th>'
-					str += (cat.size1) ? '<th>' + cat.size1 + '</th>' : ''
-					str += (cat.size2) ? '<th>' + cat.size2 + '</th>' : ''
-					str += (cat.size3) ? '<th>' + cat.size3 + '</th>' : ''
-					str += (cat.size4) ? '<th>' + cat.size4 + '</th>' : ''
-					str += (cat.size5) ? '<th>' + cat.size5 + '</th>' : ''
-					str += '</tr>'
+			$.each(categories, function(i, cat) {
+				var str = '<div id="collapsible"><h3>' + cat.name + '</h3><div id="content">'
+				str += '<table><tr>'
+				str += '<th>Name</th>'
+				str += (cat.size1) ? '<th>' + cat.size1 + '</th>' : ''
+				str += (cat.size2) ? '<th>' + cat.size2 + '</th>' : ''
+				str += (cat.size3) ? '<th>' + cat.size3 + '</th>' : ''
+				str += (cat.size4) ? '<th>' + cat.size4 + '</th>' : ''
+				str += (cat.size5) ? '<th>' + cat.size5 + '</th>' : ''
+				str += '</tr>'
 
-					$.each(menu, function(j, item) {
-						if (item.category == cat.name) {
-							str += '<tr><td>' + item.name + '</td>'
-							str += (item.price1) ? '<td>' + item.price1 + '</td>' : ''
-							str += (item.price2) ? '<td>' + item.price2 + '</td>' : ''
-							str += (item.price3) ? '<td>' + item.price3 + '</td>' : ''
-							str += (item.price4) ? '<td>' + item.price4 + '</td>' : ''
-							str += (item.price5) ? '<td>' + item.price5 + '</td>' : ''
-							str += '</tr>'
-						}
-					})
-					
-					str += '</table>'
-					str += '</div></div>'
-
-					output.append(str)
+				$.each(menu, function(j, item) {
+					if (item.category == cat.name) {
+						str += '<tr><td>' + item.name + '</td>'
+						str += (item.price1) ? '<td>' + item.price1 + '</td>' : ''
+						str += (item.price2) ? '<td>' + item.price2 + '</td>' : ''
+						str += (item.price3) ? '<td>' + item.price3 + '</td>' : ''
+						str += (item.price4) ? '<td>' + item.price4 + '</td>' : ''
+						str += (item.price5) ? '<td>' + item.price5 + '</td>' : ''
+						str += '</tr>'
+					}
 				})
-			}
+				
+				str += '</table>'
+				str += '</div></div>'
+
+				output.append(str)
+			})
 		}
 	})
 ////////////////////////////////////////////////////////////////////
