54337144eea4e9aa472735c3d269701e8caf8093
--unset <--unset>
2015-08-28 23:36:06 -0700

    Adding internetless connecting

--- js/home_aromas.js
+++ js/home_aromas.js
@@ -42,6 +42,9 @@ function fillTables() {
 		jsonp: 'jsoncallback',
 		timeout: 0,
 		success: function(data, status) {
+			var categories = []
+			var menu = []
+
 			$.each(data[1], function(i, item) { 	// assigns data[1][i] to item
 				categories.push({
 					name: item.name,  
@@ -66,37 +69,39 @@ function fillTables() {
 			})
 		},
 		error: function() {
-			alert("Please connect to an internet connection for up to date dining information")
+			output.append('<p>Please connect to an internet connection for up to date dining information</p>')
 		},
 		complete: function() {
-			$.each(categories, function(i, cat) {
-				var str = '<div id="collapsible"><h3>' + cat.name + '</h3><div id="content">'
-				str += '<table><tr>'
-				str += '<th>Name</th>'
-				str += (cat.size1) ? '<th>' + cat.size1 + '</th>' : ''
-				str += (cat.size2) ? '<th>' + cat.size2 + '</th>' : ''
-				str += (cat.size3) ? '<th>' + cat.size3 + '</th>' : ''
-				str += (cat.size4) ? '<th>' + cat.size4 + '</th>' : ''
-				str += (cat.size5) ? '<th>' + cat.size5 + '</th>' : ''
-				str += '</tr>'
+			if (menu && categories) {
+				$.each(categories, function(i, cat) {
+					var str = '<div id="collapsible"><h3>' + cat.name + '</h3><div id="content">'
+					str += '<table><tr>'
+					str += '<th>Name</th>'
+					str += (cat.size1) ? '<th>' + cat.size1 + '</th>' : ''
+					str += (cat.size2) ? '<th>' + cat.size2 + '</th>' : ''
+					str += (cat.size3) ? '<th>' + cat.size3 + '</th>' : ''
+					str += (cat.size4) ? '<th>' + cat.size4 + '</th>' : ''
+					str += (cat.size5) ? '<th>' + cat.size5 + '</th>' : ''
+					str += '</tr>'
 
-				$.each(menu, function(j, item) {
-					if (item.category == cat.name) {
-						str += '<tr><td>' + item.name + '</td>'
-						str += (item.price1) ? '<td>' + item.price1 + '</td>' : ''
-						str += (item.price2) ? '<td>' + item.price2 + '</td>' : ''
-						str += (item.price3) ? '<td>' + item.price3 + '</td>' : ''
-						str += (item.price4) ? '<td>' + item.price4 + '</td>' : ''
-						str += (item.price5) ? '<td>' + item.price5 + '</td>' : ''
-						str += '</tr>'
-					}
+					$.each(menu, function(j, item) {
+						if (item.category == cat.name) {
+							str += '<tr><td>' + item.name + '</td>'
+							str += (item.price1) ? '<td>' + item.price1 + '</td>' : ''
+							str += (item.price2) ? '<td>' + item.price2 + '</td>' : ''
+							str += (item.price3) ? '<td>' + item.price3 + '</td>' : ''
+							str += (item.price4) ? '<td>' + item.price4 + '</td>' : ''
+							str += (item.price5) ? '<td>' + item.price5 + '</td>' : ''
+							str += '</tr>'
+						}
+					})
+					
+					str += '</table>'
+					str += '</div></div>'
+
+					output.append(str)
 				})
-				
-				str += '</table>'
-				str += '</div></div>'
-
-				output.append(str)
-			})
+			}
 		}
 	})
 ////////////////////////////////////////////////////////////////////
